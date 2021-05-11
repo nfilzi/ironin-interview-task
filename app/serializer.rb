249aa37b5e7261bf9a0ae2f8003e6d6c5ca07612
Nicolas Filzi <nfilzi.webservices@gmail.com>
2021-05-11 16:56:51 +0200

    Add parent Serializer and manual FoobarSerializer test

--- app/serializer.rb
+++ app/serializer.rb
@@ -1,2 +1,35 @@
+# require 'pry-byebug'
+require 'date'
+
 class Serializer
+  class << self
+    def attribute(name, &block)
+      @attributes ||= {}
+      @attributes[name] = block
+    end
+
+    def attributes
+      return @attributes
+    end
+  end
+
+  attr_reader :object
+
+  def initialize(object)
+    @object = object
+  end
+
+  def serialize
+    data = {}
+
+    self.class.attributes.each do |key, proc|
+      if proc
+        data[key] = instance_eval(&proc)
+      else
+        data[key] = object.public_send(key)
+      end
+    end
+
+    return data
+  end
 end
---
+++ app/test.rb
@@ -0,0 +1,13 @@
+require_relative 'serializer'
+
+class FoobarSerializer < Serializer
+  attribute :id
+  attribute :date do
+    object.date.strftime("%d-%m-%Y")
+  end
+end
+
+Foobar = Struct.new(:id, :date)
+
+p FoobarSerializer.new(Foobar.new(1, Date.new(2020, 1, 1))).serialize
+# => {:id=>1, :date=>"01-01-2020"}
